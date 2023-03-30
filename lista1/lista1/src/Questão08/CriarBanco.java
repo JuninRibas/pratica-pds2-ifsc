64347de526a0d5723da9900649c211ce7872d529
Ezequiel Ribas Figueira Junior <juniorfigueira0800@gmail.com>
2023-03-29 22:55:01 -0300

    Questão08

---
+++ lista1/lista1/src/Questão08/CriarBanco.java
@@ -0,0 +1,49 @@
+package Questão08;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CriarBanco {
+	private static final String USER = "root";
+	private static final String PSW = "JJeess3344.";
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+
+		try {
+			// Conexão com o banco de dados
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/", USER, PSW);
+
+			// : Criação do banco de dados
+			stmt = conn.createStatement();
+			String sql = "CREATE DATABASE IF NOT EXISTS minha_db";
+			stmt.executeUpdate(sql);
+			System.out.println("Banco de dados criado com sucesso!");
+
+			sql = "Use minha_db";
+			stmt.executeUpdate(sql);
+
+			// Criação da tabela
+			sql = "CREATE TABLE IF NOT EXISTS minha_tabela (" + "id INT PRIMARY KEY AUTO_INCREMENT,"
+					+ "nome VARCHAR(50)," + "idade INT)";
+			stmt.executeUpdate(sql);
+			System.out.println("Tabela criada com sucesso!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
